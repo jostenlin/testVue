9411c72fc9a4c77ff3e0fac39b026442e3fd823f
jostenlin <jostenlin.gmail.com>
2022-10-16 00:03:54 +0800

    ok

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,54 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Hello from "./pages/Hello.vue";
+import { getCurrentUser } from "@/plugins/firebase";
 
 const routes = [
   {
     path: "/",
     component: Hello,
-  }
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
+    meta: { requireAuth: true },
+  },
+  {
+    path: "/secret",
+    name: "secret",
+    component: () => import("../views/SecretView.vue"),
+    meta: { requireAuth: true },
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("../views/RegisterView.vue"),
+    meta: { redirectIfAuthenticated: true },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/LoginView.vue"),
+    meta: { redirectIfAuthenticated: true },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory('/test/'),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const authStatus = await getCurrentUser();
+  const requireAuth = to.matched.some((route) => route.meta.requireAuth);
+
+  if (requireAuth && !authStatus) {
+    next({ name: "login" });
+  } else if (to.meta.redirectIfAuthenticated && authStatus) {
+    next({ name: "home" });
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ src/stores/auth.js
@@ -0,0 +1,64 @@
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+} from "firebase/auth";
+import { auth } from "@/plugins/firebase";
+
+export const useAuthStore = defineStore({
+    id: "auth",
+
+    state: () => ({
+        user: null,
+        isLoggedIn: false,
+    }),
+
+    getters: {
+        getCurrentUser: () => auth.currentUser,
+    },
+
+    actions: {
+        register(data) {
+            return new Promise((resolve, reject) => {
+                createUserWithEmailAndPassword(auth, data.email, data.password)
+                    .then(async (userCredentials) => {
+                        this.user = userCredentials.user;
+                        this.router.push("/");
+                        resolve(userCredentials);
+                    })
+                    .catch((error) => {
+                        console.log("An error happened.", error);
+                        reject(error);
+                    });
+            });
+        },
+
+        login(data) {
+            return signInWithEmailAndPassword(auth, data.email, data.password);
+        },
+
+        logout() {
+            signOut(auth)
+                .then(() => {
+                    this.router.push("/login");
+                })
+                .catch((error) => {
+                    console.log("An error happened.", error);
+                });
+        },
+
+        initializeAuthentication() {
+            const user = this.getCurrentUser;
+
+            if (user) {
+                this.user = user;
+                this.isLoggedIn = true;
+            } else {
+                this.user = null;
+                this.isLoggedIn = false;
+            }
+        },
+    },
+});
+
